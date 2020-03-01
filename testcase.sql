c261d9a82df3e08f5844cddb9a937486a88cd3bf
Victoooooor <43017658+Victoooooor@users.noreply.github.com>
2020-03-01 18:05:56 -0500

    Add files via upload

---
+++ testcase.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERTB (email,username,password) VALUES("victorzh716@gmail.com","victor","password"),("victorz.beijing@gmail.com","victor","password"),("zhang20@cooper.edu","victor","password"),("zzh20000716@icloud.com","victor","password"),("zzh20000716@126.com","victor","password"),("victorz.cu@hotmail.com","victor","password"),("jlam@cooper.edu","john","*&^%E(&%^$#*&%*(%($#^%&%*(&(TR*&$*&TU*&$%&^$^#"),("richkid@cooper.edu","richard","wooolala"),("ti@cooper.edu","tiffany?","password");
+INSERT INTO CLLECTB (pub) VALUES(0),(1),(1),(0),(1);
+INSERT INTO ImageTB (uri,upload_date,description,uid,orig_uid) VALUES("/cat.jpg", "990912","this is a cat",7,1),("/gilbert.jpg", "990912","this is ???",9,2),("/flower.jpg", "990912","this is a Richard",8,3);
+INSERT INTO ACL (cid,uid,role) VALUES(1,5,0),(1,1,2),(1,2,1),(2,6,2);
+INSERT INTO ICJ (pid,cid) VALUES(1,1),(1,2),(1,3),(2,2);
