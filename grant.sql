29f9821edb9d33bf13be6ef9e1e0f30d1d429799
VICTOR <victorz.beijing@gmail.com>
2020-04-21 08:42:23 -0400

    local:6600

--- grant.sql
+++
@@ -1,4 +0,0 @@
-GRANT USAGE ON *.* TO 'photo_server'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL privileges ON `photocol`.* TO 'photo_server'@localhost;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'hGbC8Qo5Wi&&CsJhb&xfh4$eVzc^5m';
-
--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mysql -u root <deleteall.sql
-mysql -u root <dbload.sql
-mysql -u root <testcase.sql
-mysql -u root <grant.sql
