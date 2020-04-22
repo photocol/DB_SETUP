726ec4112733644a27812e818fb187d26242e378
VICTOR <victorz.beijing@gmail.com>
2020-04-22 02:03:02 -0400

    clear change

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-MCN = $MYSQL_CONTAINER_NAME
-DB = $MYSQL_DATABASE
-MU = $MYSQL_USER
-MP = $MYSQL_PASSWORD
+MCN=$MYSQL_CONTAINER_NAME
+DB=$MYSQL_DATABASE
+MU=$MYSQL_USER
+MP=$MYSQL_PASSWORD
 
 [ ! -d /backup ] && mkdir --parents /backup
-DT = $(date +%d)
-MD = $(which mysqldump)
-GZ = $(which gzip)
+DT=$(date +%d)
+MD=$(which mysqldump)
+GZ=$(which gzip)
 $MD -h $MCN -u $MU -p $MP --databases $MB | $GZ -9 > /backup/D-$DT.sql.gz
+
+RM=/bin/rm;
+XARGS=/usr/bin/xargs;
+FIND=/usr/bin/find;
+$FIND /backup -mtime +7 |$XARGS $RM -rf
