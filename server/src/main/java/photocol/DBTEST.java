eff31db51379d2bee864df4256534ace26a8693a
VICTOR <victorz.beijing@gmail.com>
2020-04-22 21:06:25 -0400

    dummy

--- server/src/main/java/photocol/DBTEST.java
+++ server/src/main/java/photocol/DBTEST.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class DBTEST {
     public static void main(String[] Args) throws SQLException, HttpMessageException {
         UserStore udb = new UserStore();
-        User Af = new User("victorgggggzh716@gmail.com", "victooorra", "pw123");
+        User Af = new User("uniquegggggzh716@gmail.com", "victoooasfgv", "pw123");
         udb.createUser(Af);
         udb.checkCredentials(Af);
 
