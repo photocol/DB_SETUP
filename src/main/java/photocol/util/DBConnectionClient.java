bbfe8bcdb17c926741b9b4dde030602cb7133af7
VICTOR <victorz.beijing@gmail.com>
2020-05-08 15:43:09 -0400

    Squashed 'server/' content from commit b2ad3d2
    git-subtree-dir: server
    git-subtree-split: b2ad3d226c3554cf236b2916a40c558c7d86c6e9

---
+++ src/main/java/photocol/util/DBConnectionClient.java
@@ -0,0 +1,29 @@
+package photocol.util;
+
+
+import java.sql.*;
+
+public class DBConnectionClient {
+    private final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    private String DB_URL = System.getenv("DB_URL");
+
+    //  Database credentials
+    /* TODO: move credentials to environment variables */
+    private final String USER = "photo_server";
+    private final String PASS = "password";
+    private Connection connection = null;
+    public DBConnectionClient() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            connection.prepareStatement("USE photocol").executeUpdate();
+        } catch (Exception err) {
+            System.err.println("Error connecting to database.");
+            err.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
