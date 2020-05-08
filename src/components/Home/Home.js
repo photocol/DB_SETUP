8592f7637c44e9b25fb75559e00c4a504b33fa45
VICTOR <victorz.beijing@gmail.com>
2020-05-08 00:51:00 -0400

    Squashed 'website/' content from commit 13bf4c8
    git-subtree-dir: website
    git-subtree-split: 13bf4c87b33871dbcac971f893c8d379f0e6e4b5

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import { Container } from 'reactstrap';
+
+const Home = () => (
+  <Container className="Home mt-5">
+    <h1>Welcome to Photocol!</h1>
+    <p>A work in progress</p>
+    <p>by JLRLTYVZ</p>
+  </Container>
+);
+
+export default Home;
