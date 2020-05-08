397fc2f54c51700617a0c21e8825a239f50753ac
VICTOR <victorz.beijing@gmail.com>
2020-05-08 19:13:46 -0400

    merge

--- script/c.sql
+++
@@ -1,6 +0,0 @@
-GRANT USAGE ON *.* TO 'photo_server'@'%' IDENTIFIED BY 'password';
-GRANT ALL privileges ON `photocol`.* TO 'photo_server'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'mypassword';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'mypassword';
-
-
