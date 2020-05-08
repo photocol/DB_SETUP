5495c7bcbe9d6d66b3cbf72089d962e42ef55dcd
VICTOR <victorz.beijing@gmail.com>
2020-05-08 00:48:42 -0400

    Squashed 'website/' content from commit 283017e
    git-subtree-dir: website
    git-subtree-split: 283017ec5aa4cd5c3cd1fdb2469936ea22714359

---
+++ src/components/Photo/Photo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Photo.css';
+import {Link, withRouter} from 'react-router-dom';
+import {env} from "../../util/Environment";
+import {Button} from "reactstrap";
+
+class Photo extends React.Component {
+  constructor(props) {
+    super(props);
+    // get params from uri
+    const {photouri} = props.match.params;
+    this.state = {
+      photouri,
+    };
+  }
+
+  render = () => {
+    return (<div>
+      <Link to='/photos'>
+        <Button color="success">Return to Photos</Button>
+      </Link>
+      <div class='fill-screen'><img className="photo333" src={`${env.serverUrl}/perma/${this.state.photouri}`}/></div>
+    </div>);
+  }
+};
+
+
+Photo.propTypes = {};
+
+Photo.defaultProps = {};
+
+export default withRouter(Photo);
