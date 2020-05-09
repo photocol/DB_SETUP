52509d159f9b18444e9105819c299129c9a99705
VICTOR <victorz.beijing@gmail.com>
2020-05-09 14:40:19 -0400

    auto

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,12 +5,12 @@ MU=$MYSQL_USER
 MP=$MYSQL_PASSWORD
 
 [ ! -d /backup ] && mkdir --parents /backup
-DT=$(date +%d)
+DT=$(date +"%d+%H+%M")
 MD=$(which mysqldump)
 GZ=$(which gzip)
-$MD -h $MCN -u $MU -p $MP --databases $MB | $GZ -9 > /backup/D-$DT.sql.gz
+$MD -h $MCN -u$MU -p$MP --databases $DB | $GZ -9 > /backup/D-$DT.sql.gz
 
 RM=/bin/rm;
 XARGS=/usr/bin/xargs;
 FIND=/usr/bin/find;
-$FIND /backup -mtime +7 |$XARGS $RM -rf
+$FIND /backup -mmin +900 |$XARGS $RM -rf
